7a79a87ddb69ae5646bb76d73ab131a5539f8e20
pjm529 <pok_nima@naver.com>
2021-12-27 17:55:52 +0900

    Update UserDAO

--- DU/src/main/java/du/user/dao/UserDAO.java
+++ DU/src/main/java/du/user/dao/UserDAO.java
@@ -3,7 +3,9 @@ package du.user.dao;
 import du.user.domain.UserVO;
 
 public interface UserDAO {
-	
+
+	public String selectPwd(String userId);
+
 	public UserVO selectUserInfo(String userId);
-	
+
 }
