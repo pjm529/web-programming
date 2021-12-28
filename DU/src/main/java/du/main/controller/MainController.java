9fcc667685a6d101e6f489d06ca6680a3bc83214
pjm529 <pok_nima@naver.com>
2021-12-28 10:02:35 +0900

    login implement

--- DU/src/main/java/du/main/controller/MainController.java
+++ DU/src/main/java/du/main/controller/MainController.java
@@ -1,44 +1,59 @@
 package du.main.controller;
 
-import java.util.Arrays;
-import java.util.Map;
+import java.io.PrintWriter;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import du.user.service.UserService;
 
 @Controller
 public class MainController {
 
 	private Logger logger = LoggerFactory.getLogger(MainController.class);
 	
-	@RequestMapping(value="/main.do", method = RequestMethod.GET)
+	@Autowired
+	private UserService userService;
+	
+	/*
+	@RequestMapping(value = "/main.do", method = RequestMethod.GET)
 	public String mainPageByGet(HttpServletRequest request) {
 
 		logger.info(request.getQueryString());
-		
+
 		return "main.html";
 	}
-	
-	@RequestMapping(value="/main.do", method = RequestMethod.POST)
-	public String mainPageByPost(HttpServletRequest request) {
+	*/
 
-		Map<String, String[]> paramMap = request.getParameterMap();
-		for(String name: paramMap.keySet()) {
-			logger.info("{} : {}", name, Arrays.toString(paramMap.get(name)));
+	@RequestMapping(value = "/main.do", method = RequestMethod.POST)
+	public String mainPageByPost(@RequestParam("user_id") String userId, @RequestParam("pwd") String userPw,
+			HttpServletResponse response) throws Exception {
+
+		response.setCharacterEncoding("euc-kr");
+		boolean isLogin = userService.isLogin(userId, userPw);
+
+		if (isLogin) {
+			return "main.html";
+		} else {
+			PrintWriter out = response.getWriter();
+			out.println("<script>");
+			out.println("alert('아이디  혹은 비밀번호가 잘못되었습니다.');");
+			out.println("location.href='loginPage.do';");
+			out.println("</script>");
+
+			return null;
 		}
-		
-		logger.info(request.getQueryString());
-		
-		return "main.html";
+
 	}
 
-	@RequestMapping(value="/login.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/loginPage.do", method = RequestMethod.GET)
 	public String loginPage() {
 		return "login.html";
 	}
--- DU/src/main/java/du/user/service/impl/UserServiceImpl.java
+++ DU/src/main/java/du/user/service/impl/UserServiceImpl.java
@@ -1,11 +1,13 @@
 package du.user.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 import du.user.dao.UserDAO;
 import du.user.service.UserService;
 
+@Service("userService")
 public class UserServiceImpl implements UserService {
 
 	@Autowired
